045d7a4fc0ef61a4385b2ef9ff2c03c6972642de
amorenochocano <amoranochocanol@enagas.es>
2025-07-08 10:43:58 +0200

    ejemplo duplicados

--- func_duplicates_pk.py
+++ func_duplicates_pk.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
-def obtener_duplicados_por_campo(df, campo):
+def obtener_duplicados_por_campos(df, campos):
     """
-    Devuelve las filas del DataFrame que tienen valores duplicados en el campo especificado.
+    Devuelve las filas del DataFrame que tienen valores duplicados en la combinación de los campos especificados.
     """
-    return df[df.duplicated(subset=[campo], keep=False)]
+    return df[df.duplicated(subset=campos, keep=False)]
 
 # Crear un DataFrame de ejemplo
 df = pd.DataFrame({
-    'id': [1, 2, 2, 3, 4, 4, 4],
-    'valor': ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    'id': [1, 2, 2, 3, 4, 4, 4,4],
+    'valor': ['a', 'b', 'c', 'd', 'e', 'f', 'g','g']
 })
 
-duplicados = obtener_duplicados_por_campo(df, 'id')
+duplicados = obtener_duplicados_por_campos(df, ['id', 'valor'])
 print(duplicados)
